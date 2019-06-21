64407363967411aca28e09c9aaa79852f16785a1
BCDev <bcdev@192-168-1-103.tpgi.com.au>
2019-06-21 13:03:12 +1000

    Adding a remix importer contract

---
+++ solidity/remixImporter.sol
@@ -0,0 +1,16 @@
+pragma solidity ^0.5.0;
+import "https://github.com/MaxWdeMon/ethereum-datetime/contracts/DateTime.sol";
+import "https://github.com/MaxWdeMon/ABTokens/solidity/Deal.sol";
+import "https://github.com/MaxWdeMon/ABTokens/solidity/Note.sol";
+import "https://github.com/MaxWdeMon/ABTokens/solidity/DateCalc.sol";
+import "https://github.com/MaxWdeMon/ABTokens/solidity/Waterfall.sol";
+import "https://github.com/MaxWdeMon/ABTokens/solidity/tests/Deal_test.sol";
+import "https://github.com/MaxWdeMon/ABTokens/solidity/tests/DealProxy_tester.sol";
+import "https://github.com/MaxWdeMon/ABTokens/solidity/tests/Waterfall_test.sol";
+import "https://github.com/MaxWdeMon/ABTokens/solidity/tests/Waterfalllib_tester.sol";
+import "https://github.com/MaxWdeMon/ABTokens/solidity/tests/DateCalc_test.sol";
+
+
+contract blank{
+    
+}
